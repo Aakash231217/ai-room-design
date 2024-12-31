c9f3eaf0a6509901e74e515f4800b11cb4e3ba0e
aakash <rktaakash@gmail.com>
2024-12-31 06:34:48 +0530

    Dashboard

--- app/dashboard/create-new/_components/DesignType.jsx
+++ app/dashboard/create-new/_components/DesignType.jsx
@@ -1,25 +1,25 @@
 import Image from 'next/image'
 import React from 'react'
 
-function DesignType() {
+function DesignType(selectedDesignType) {
     const Designs=[
         {
             name:'Modern',
             image:'/star.png',
         }
     ]
+    const [selectedOption, setSelectedOption] = React.useState();
   return (
-    <div>
+    <div className='mt-5'>
         <label className='text-lg font-medium text-gray-500'>Select Interior Design Type</label>
-        <div className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5'>
+        <div className='grid grid-cols-2 mt-3 md:grid-cols-3 lg:grid-cols-4 gap-5'>
             {Designs.map((design,index)=>(
-                <div key={index} className='flex items-center justify-between mt-5'>
+                <div key={index} onClick={()=>{setSelectedOption(design.name); selectedDesignType(design.name)}}>
                     <div>
-                        <Image src={design.image} alt={design.name} className='w-100 h-100'/>
+                        <Image src={design.image} width={100} height={100} className={`h-[70px] rounded-md hover:scale-105 transition-all cursor-pointer ${design.name==selectedOption && 'border-2 border-primary rounded-md'}`}/>
+                        <h2>{design.name}</h2>
                     </div>
-                    <div>
-                        <h3 className='text-lg font-medium'>{design.name}</h3>
-                    </div>
+                    <div>                    </div>
                 </div>
             ))}
         </div>
--- app/dashboard/create-new/page.jsx
+++ app/dashboard/create-new/page.jsx
@@ -1,11 +1,15 @@
 "use client"
 import React from 'react'
+import { Button } from '@/components/ui/button'
 import ImageSelection from './_components/ImageSelection'
 import RoomType from './_components/RoomType'
-
+import DesignType from './_components/DesignType'
+import AdditionalReq from './_components/AdditionalReq'
 function CreateNew() {
+    const[formData,setFormData]=React.useState([]);
     const onHandleInputChange=(value,fieldName)=>{
-        console.log(value,fieldName)
+        setFormData(prev=>({...prev,[fieldName]:value}))
+        
 
     }
   return (
@@ -21,11 +25,14 @@ function CreateNew() {
             <RoomType  selectedRoomType={(value)=>onHandleInputChange(value,'roomType')}/>
 
             {/* Design type*/}
+            <DesignType selectedDesignType={(value)=>onHandleInputChange(value,'designType')}/>
 
             {/* Additional TextArea*/}
+            <AdditionalReq additionalRequirementInput={(value)=>onHandleInputChange(value,'additionalReq')}/>
 
             {/*Button to generate image*/}
-
+            <Button className="w-full mt-5">Generate</Button>
+            <p className='text-sm text-gray-400 mb-52'>Note: 1Credit will be used to generate to redesign your room</p>
          </div>
        </div>
    
